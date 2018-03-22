480906e2fa9d87e8b921b58c8b2b56b9c04f5935
Crisna Lek 2760 <5urp@fresnou.org>
2018-03-22 09:38:33 -0700

    code
    3/22/18

---
+++ Item.py
@@ -0,0 +1,45 @@
+class Item(object):
+    def __init__(self, weapon, consumable):
+        self.weapon = weapon
+        self.consumable = consumable
+
+class Weapons(object):
+    def __init__(self, weapon, knife, gun, ar15, scythe):
+        super(Item, self, weapon, knife, gun, ar15, scythe)
+
+class Consumables(object):
+    def __init__(self, bread, water):
+        super(Weapons, self, bread, water)
+        self.bread = bread
+        self.water = water
+
+class Defense(object):
+    def __init__(self, shield, armor, potion):
+        super(Item, self, shield, armor, potion)
+        self.shield = shield
+        self.armor = armor
+        self.potion
+
+class Knife(object):
+    def __init__(self, description, damage):
+        super(Item, self, description, damage)
+        self.description = description
+
+    def pick_up(self):
+        if Knife.pick_up:
+            print('You do not pick knife up')
+        else:
+            self.pick_up = True
+            print('You do not pick up the knife')
+
+class AR15(object):
+    def __init__(self, description):
+        super(Item, self, description)
+
+class Key(object):
+    def __init__(self, description):
+        super(Item, self, description)
+
+class Chest_Key(object):
+    def __init__(self, description):
+        super(Item, description)
